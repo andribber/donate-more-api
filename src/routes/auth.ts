115d3a0bfafd826c2006b12fc32500f73e053be5
Lucas Prado <lucasntc2201@gmail.comq:qq>
2023-05-25 21:12:22 -0300

    feat: register route working

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,91 +3,78 @@ import { FastifyInstance } from 'fastify'
 import axios from 'axios'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
-import { v4 as uuidv4 } from 'uuid'
 
 //https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20profile%20email&client_id=927884961647-hbuhl6781r59lcorsb9jn12u7jpjgr8f.apps.googleusercontent.com&redirect_uri=http://localhost:3333/auth/callback
 
-const userSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  given_name: z.string(),
-  family_name: z.string(),
-  picture: z.string().url(),
-  locale: z.string(),
-  email: z.string(),
-})
-
-const bodySchema = z.object({
-  code: z.string(),
-})
-
 export async function authRoutes(app: FastifyInstance) {
-  app.get('/auth', async (request, reply) => {
-    const redirectUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20profile%20email&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`
+  // app.get('/auth', async (request, reply) => {
+  //   const redirectUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20profile%20email&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`
 
-    reply.redirect(redirectUrl)
-  })
+  //   reply.redirect(redirectUrl)
+  // })
 
-  app.post('/register', async (request: any, reply) => {
+  app.post('/register', async (request, reply) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
     const { code } = bodySchema.parse(request.body)
 
-    const accessTokenResponse = await axios
-      .post(process.env.GOOGLE_TOKEN_URI!, {
-        code: code,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: process.env.REDIRECT_URI,
-        grant_type: 'authorization_code',
+    const accessTokenResponse = await axios.post(
+      'https://accounts.google.com/o/oauth2/token', {
+      code: code,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: 'authorization_code',
+    })
+
+    const userInfoResponse = await axios
+      .get('https://www.googleapis.com/oauth2/v1/userinfo', {
+        headers: {
+          Authorization: `Bearer ${accessTokenResponse.data.access_token}`,
+        },
       })
-      .then((response) => {
-        axios
-          .get(process.env.GOOGLE_USER_INFO_URI!, {
-            headers: {
-              Authorization: `Bearer ${response.data.access_token}`,
-            },
-          })
-          .then(async (res) => {
-            const userInfo = userSchema.parse(res.data)
 
-            let user = await prisma.user.findUnique({
-              where: {
-                google_id: userInfo.id,
-              },
-            })
+    const userSchema = z.object({
+      id: z.string(),
+      name: z.string(),
+      given_name: z.string(),
+      family_name: z.string(),
+      picture: z.string().url(),
+      email: z.string(),
+    })
 
-            if (!user) {
-              user = await prisma.user.create({
-                data: {
-                  id: uuidv4(),
-                  google_id: userInfo.id,
-                  first_name: userInfo.given_name,
-                  last_name: userInfo.family_name,
-                  email: userInfo.email,
-                  picture_url: userInfo.picture,
-                  phone_number: '1234567', //TODO ajustar para ser solicitado em um front e guardado no banco
-                },
-              })
-            }
+    const userInfo = userSchema.parse(userInfoResponse.data)
 
-            const token = app.jwt.sign(
-              {
-                name: user.first_name,
-                gid: user.google_id,
-              },
-              {
-                sub: user.id,
-                expiresIn: '30 days',
-              }
-            )
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id,
+      },
+    })
 
-            return { token }
-          })
-          .catch((err) => {
-            return reply.send(err)
-          })
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.given_name.concat(' ').concat(userInfo.family_name),
+          email: userInfo.email,
+          avatarUrl: userInfo.picture,
+        },
       })
-      .catch((err) => {
-        return reply.send(err)
-      })
+    }
+
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      }
+    )
+
+    return { token }
   })
 }
